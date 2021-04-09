db76f645bf76dc495fc22550142959c4db4ea349
sathya9997 <82139187+sathya9997@users.noreply.github.com>
2021-04-09 12:31:10 +0530

    three methods have been updated (#1)
    your project has been merged.dnt worry. wil take care

--- src/org/locator/Demo.java
+++ src/org/locator/Demo.java
@@ -4,6 +4,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo {
+	
+	
+	private void iphones() {
+		System.out.println("iphone 12 pro max");
+	}
+	
+	private void samsung() {
+		System.out.println("samsung s12 ");
+	}
+	
+	private void oneplus() {
+		System.out.println("oneplus 9");
+	}
+	
+	
+	
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\vImal\\EclipSelenium-2\\LocatorDay2\\Drivers\\chromedriver.exe");
